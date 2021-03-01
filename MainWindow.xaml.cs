969c77d263a83be13ad9bd110fa458813e5d37da
Zakaria <r0841986@student.vives.be>
2021-03-01 14:24:39 +0100

    Rekenkundige bewerkingen plus

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,18 @@ namespace GithubRekenen
         {
             InitializeComponent();
         }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            int value1 = 0;
+            int value2 = 0;
+            int result = 0;
+
+            if (int.TryParse(txtNumber1plus.Text, out value1) & int.TryParse(txtNumber2plus.Text, out value2))
+            {
+                result = value1 + value2;
+                lblResultPlus.Content = result.ToString();
+            }
+        }
     }
 }
